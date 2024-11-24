8962a27558e0a53c78857c210daecd8069cc2926
Marco <mark99961@gmail.com>
2024-11-24 16:50:25 +0100

    Parte 2 finita, aggiunta su ransac_code dell' abs(residuals)<,> than th, in attesa di chiedere conferma a prof

--- input/ransac_code.m
+++ input/ransac_code.m
@@ -19,17 +19,19 @@ function [bestF, consensus, outliers] = ransacF(P1, P2, th)
         P1iter = P1(:, perm(1:8)); % select 8 random pairs
         P2iter = P2(:, perm(1:8)); % select 8 random pairs
 
-        F = EightPointsAlgorithmN(P1iter, P2iter);
+        F = EightPointsAlgorithm_v2(P1iter', P2iter');
 
         residuals = testF(F, P1, P2); 
+        fprintf('Iterazione %d:',ii);
+        disp(residuals);
 
-        nInlier = sum(residuals < th) / N; % current estimated inliers
-
+        nInlier = sum(abs(residuals) < th) / N; % current estimated inliers
+        disp(sum(abs(residuals) < th))
         if(nInlier > bestNInlier) % if number of inliers grows
            bestNInlier = nInlier; % update of the fly the probability of having inliers
            bestF = F; % update the best fundamental matrix
-           consensus = [P1(:, residuals < th); P2(:, residuals < th)]; % inliers
-           outliers = [P1(:, residuals >= th); P2(:, residuals >= th)]; % outliers
+           consensus = [P1(:, abs(residuals) < th); P2(:, abs(residuals) < th)]; % inliers
+           outliers = [P1(:, abs(residuals) >= th); P2(:, abs(residuals) >= th)]; % outliers
            iter = log(1 - p) / log(1 - bestNInlier.^4); % update the number of iterations needed
         end
         ii = ii + 1;
--- main1.m
+++ main1.m
@@ -3,6 +3,7 @@ clc; clear all; close all;
 addpath("Mire\Mire\")
 addpath("input\")
 
+%% Part_1
 P1 = load("Mire\Mire\Mire1.points");
 P2 = load("Mire\Mire\Mire2.points");
 
@@ -16,5 +17,28 @@ F_v2 = EightPointsAlgorithm_v2(P1_norm,P2_norm);
 check_F(P1_norm', P2_norm', F_v1);
 check_F(P1_norm', P2_norm', F_v2)
 
+%% Part_2
+
+img_1 = imread('Mire\Mire\Mire1.pgm');
+img_2 = imread('Mire\Mire\Mire2.pgm');
+
+M = findMatches(img_1, img_2, 'SIFT', 0.8);
+show_matches(img_1, img_2, M, 0, 10);
+
+P1 = M(:,1:2);
+P2 = M(:,3:4);
+
+P1_norm = [P1, ones(size(P1,1),1)];
+P2_norm = [P2, ones(size(P2,1),1)];
+
+P1_norm = P1_norm';
+P2_norm = P2_norm';
+
+[bestF, consensus, outliers] = ransac_code(P1_norm, P2_norm, 0.1);
+
+P1_cons = consensus(1:3,:);
+P2_cons = consensus(4:6,:);
+
+check_F(P2_cons, P1_cons, bestF);
 
 
